153929bb0a1fb0061b1c2e2efa92939fb34eed96
辻昌紘 <tsujiakihiro@tsujimayuminoMacBook-puro.local>
2019-03-20 19:07:58 +0900

    WebSite

---
+++ Project/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+//
+//	@Autowired
+//	TaskRepository taskRepo;
+
+//	@ModelAttribute("sampleForm")
+//    public SampleForm setupForm() {
+//        SampleForm sample = new SampleForm();
+//        return sample;
+//    }
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index(ModelAndView model) {
+		// TODOリスト取得
+//		List<Task> taskList = taskRepo.findAll();
+
+		// 取得したリストをモデルに設定
+//		model.addObject("taskList", taskList);
+
+		// 画面描画用のテンプレート名を指定
+		model.setViewName("tasks");
+		return model;
+	}
+}
